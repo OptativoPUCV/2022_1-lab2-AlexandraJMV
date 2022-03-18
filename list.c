d99fb689b1848d077a68829b66091e0e8254c023
AlexandraJMV <>
2022-03-18 03:22:59 +0000

    new try

--- list.c
+++ list.c
@@ -52,15 +52,7 @@ void * firstList(List * list) {
 }
 
 void * nextList(List * list) {
-	if (list->current == NULL)
-		exit (1);
-	else if (list->current->next == NULL)
-			exit(1);
-		else
-		{
-			list->current = list->current->next;
-		}
-	return list->current->data;
+	return NULL;
 }
 
 void * lastList(List * list) {
